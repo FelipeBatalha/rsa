5e04728e1aa2c73bcca62ec4fe1154270d093681
Felipe Batalha <felipe.batalha@ish.com.br>
2023-11-29 14:05:58 -0300

    miller rabin improvements

--- key-generation.py
+++ key-generation.py
@@ -8,8 +8,13 @@ print(p)
 n = p * q
 
 def miller_rabin(n):
+
+    if n % 2 == 0:
+        return False
+    
     r = 0
     d = n - 1
+
     while d % 2 == 0:
         d //= 2
         r += 1
@@ -17,24 +22,22 @@ def miller_rabin(n):
     a = random.randint(1, n)
     x = pow(a, d, n)
 
-    k = 1000
+    k = 1000000
     for i in range(1, k):
         a = random.randint(1, n)
         print(f"O a é {a}")
         x = pow(a, d, n)
         print(f"O x é {x}")
+
         if (x == 1 % n) or (x == -1 % n):
-            print("Pode ser primo ainda")
-            pass    #cuz it could be prime
+            print("Pode ser primo")
         else:
             for j in range(1, r - 1):
                 x = pow(x, 2, n)
                 if x == 1 % n:
-                    print(f"j: {j}")
-                    print("É composto, game over")
+                    print("É composto")
                     return False    #cuz its composite
-                if x == -1 % n:
-                    print("Pode ser primo ainda")
-                    pass    #cuz it could be prime
+                elif x == -1 % n:
+                    print("Pode ser primo")
 
-miller_rabin(324319)
+miller_rabin(2221)

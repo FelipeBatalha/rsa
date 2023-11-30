2790a2ca1336950770a60e845a163ac4447633de
FelipeBatalha <felipebatalha96@gmail.com>
2023-11-30 00:27:34 -0300

    trying to generate two primes

--- key-generation.py
+++ key-generation.py
@@ -1,11 +1,6 @@
 import random
-
-p = random.getrandbits(512)
-q = random.getrandbits(512)
-
-print(p)
-
-n = p * q
+import threading
+from queue import Queue
 
 def miller_rabin(n):
 
@@ -22,22 +17,58 @@ def miller_rabin(n):
     a = random.randint(1, n)
     x = pow(a, d, n)
 
-    k = 1000000
+    k = 100000000
     for i in range(1, k):
         a = random.randint(1, n)
-        print(f"O a é {a}")
+        #pritn(f"O a é {a}")
         x = pow(a, d, n)
-        print(f"O x é {x}")
+        #pritn(f"O x é {x}")
 
         if (x == 1 % n) or (x == -1 % n):
-            print("Pode ser primo")
+            pass
+            #pritn("Pode ser primo")
         else:
             for j in range(1, r - 1):
                 x = pow(x, 2, n)
                 if x == 1 % n:
-                    print("É composto")
+                    #pritn("É composto")
                     return False    #cuz its composite
                 elif x == -1 % n:
-                    print("Pode ser primo")
+                    pass
+                    #pritn("Pode ser primo")
+    return True
 
-miller_rabin(2221)
+def get_prime_number(queue):
+    is_prime = False
+    while is_prime is False:
+        if is_prime:
+            break
+        else:
+            number = random.getrandbits(128)
+            is_prime = miller_rabin(number)
+    queue.put(number)
+
+def main():
+
+    result_queue_p = Queue()
+    result_queue_q = Queue()
+
+    p_thread = threading.Thread(target=get_prime_number,args=(result_queue_p,))
+    q_thread = threading.Thread(target=get_prime_number, args=(result_queue_q,))
+
+    p_thread.start()
+    q_thread.start()
+
+    p_thread.join()
+    q_thread.join()
+
+    p = result_queue_p.get()
+    q = result_queue_q.get()
+
+    print(f"O p é {p}")
+    print(f"O q é {q}")
+
+    n = p * q
+    print(n)
+
+main()

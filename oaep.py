7978b9fdc640130da97c06cb766c9e23f3c67c3f
FelipeBatalha <felipebatalha96@gmail.com>
2023-12-06 01:14:20 -0300

    fixing oaep encryption

--- oaep.py
+++ oaep.py
@@ -5,6 +5,22 @@ import base64
 import hashlib
 from rsa import *
 
+def mgf1(seed: bytes, length: int, hash_func=hashlib.sha3_256) -> bytes:
+    """Mask generation function."""
+    hLen = hash_func().digest_size
+    
+    if length > (hLen << 32):
+        raise ValueError("mask too long")
+    
+    T = b""
+    
+    counter = 0
+    while len(T) < length:
+        C = int.to_bytes(counter, 4, "big")
+        T += hash_func(seed + C).digest()
+        counter += 1
+    return T[:length]
+
 def hash(message):
     if isinstance(message, str):
         message = message.encode()
@@ -26,28 +42,35 @@ def oaep_encrypt(message, public_key, private_key):
 
     hash_length = hashlib.sha256().digest_size 
 
-    size_limit = len(message_bytes) > k - 2 * hash_length - 2
+    size_limit = (len(message_bytes) > k - 2 * hash_length - 2)
 
     if size_limit:
-        raise ValueError("Message too long for OAEP padding")
+        raise ValueError("File too long for OAEP padding")
     
-    padding_length = k - size_limit
+    padding_length = k - len(message_bytes) - 2 * hash_length - 2
     padding = b'\x00' * padding_length + b'\x01'
+    data_block = padding + message_bytes
     
-    #random nonce
+    
     seed = os.urandom(hash_length)
+    data_block_length = k - hash_length - 1
+    data_block_mask = mgf1(seed, data_block_length)
+
+    masked_data_block = int.from_bytes(data_block, 'big') ^ int.from_bytes(data_block_mask, 'big')
+    masked_data_block = masked_data_block.to_bytes(data_block_length, 'big')
 
     #mask for the message
-    seed_hash = hash(seed)
-    masked_seed = int.from_bytes(seed, 'big') ^ int.from_bytes(seed_hash, 'big')
+    seed_mask = mgf1(masked_data_block, hash_length) #pode precisar ver se é bytes e tal
+    masked_seed = int.from_bytes(seed, 'big') ^ int.from_bytes(seed_mask, 'big')
     masked_seed = masked_seed.to_bytes(hash_length, 'big')
     
     #OAEP padding
     
-    padded_message = padding + masked_seed + hash(masked_seed + message_bytes) + message_bytes
+    message_to_encrypt = b'\x00' + masked_seed + masked_data_block
 
     # RSA encryption
-    encrypted = pow(int.from_bytes(padded_message, 'big'), e, n)
+    encrypted = pow(int.from_bytes(message_to_encrypt, 'big'), e, n)
+    print(encrypted)
     oaep_decrypt(encrypted, private_key)
     return encrypted
 
